a9c119669d12ed995d6d39139bc7970bca495092
LoleeconDestroyer <66286710+quezera97@users.noreply.github.com>
2024-01-18 19:15:29 +0800

    remove unwanted href

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header>
       <nav>
         <ul>
-          <li><Link to="/portfolio"><a href="#" className="White-text Eerie-text Normal-text">Portfolio</a></Link></li>
+          <li><Link to="/portfolio"><p className="White-text Eerie-text Normal-text">Portfolio</p></Link></li>
           <li><a className="White-text Eerie-text Normal-text" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">YouTube</a></li>
           <li><a className="White-text Eerie-text Normal-text" href="https://www.youtube.com/watch?v=DSG53BsUYd0" target="_blank" rel="noopener noreferrer">DuckDuckGo</a></li>
         </ul>
